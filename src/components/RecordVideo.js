47004bea978222e5a85d892fbfa85731bfdd19bf
derinsolao <deeokunoren@gmail.com>
2023-09-11 17:00:55 +0200

    fixes

--- src/components/RecordVideo.js
+++ src/components/RecordVideo.js
@@ -30,41 +30,39 @@ function RecordVideo(props) {
     <div id='main-container' className="mx-2 text-white px-10">
       <p>Camera status: {status}</p>
   
-    <div id='buttons_and_preview' className="">
-    <div id='buttons' className="my-10">
-
-    {
-status!=="CLOSED" && (
-  <div id="video_container" className="absolute w-[45%] ml-[358px] top-[180px]">
-  <video ref={recordWebcam.webcamRef} autoPlay muted />
-    <video ref={recordWebcam.previewRef} autoPlay muted loop />
-  </div>
-)
-}
+    <div id='buttons_and_preview' className="mt-10 flex flex-row">
+    <div id='buttons' className="flex-column my-10">
 
   {status!=="CLOSED" ? (
-    <div className="ml-[70pt] pl-20 mt-[460px]">
-  <div className="flex-row">
-  <button className="bg-green-400 py-2 rounded-[20px] font-bold text-[16px] text-black w-[25%] my-2 mx-2" onClick={recordWebcam.close}>CLOSE CAMERA</button>
-  <button className="bg-green-400 py-2 rounded-[20px] font-bold text-[16px] text-black w-[25%] my-2 mx-2" onClick={recordWebcam.start}>START RECORDING</button>
-  <button className="bg-green-400 py-2 rounded-[20px] font-bold text-[16px] text-black w-[25%] my-2 mx-2" onClick={recordWebcam.stop}>STOP RECORDING</button>
+  <div className="ml-20">
+  <button className="bg-green-400 py-3 rounded-[20px] font-bold text-[16px] text-black w-[70%] my-2" onClick={recordWebcam.close}>CLOSE CAMERA</button>
+  <button className="bg-green-400 py-3 rounded-[20px] font-bold text-[16px] text-black w-[70%] my-2" onClick={recordWebcam.start}>START RECORDING</button>
+  <button className="bg-green-400 py-3 rounded-[20px] font-bold text-[16px] text-black w-[70%] my-2" onClick={recordWebcam.stop}>STOP RECORDING</button>
+  <button className="bg-green-400 py-3 rounded-[20px] font-bold text-[16px] text-black w-[70%] my-2" onClick={recordWebcam.retake}>RETAKE RECORDING</button>
+  <button className="bg-green-400 py-3 rounded-[20px] font-bold text-[16px] text-black w-[70%] my-2" onClick={recordWebcam.download}>DOWNLOAD RECORDING</button>
+  <button className="bg-green-400 py-3 rounded-[20px] font-bold text-[16px] text-black w-[70%] my-2" onClick={saveFile}>SAVE FILE TO SERVER</button>
 
-</div>
- <div className="flex-row">
- <button className="bg-green-400 py-2 rounded-[20px] font-bold text-[16px] text-black w-[25%] my-2 mx-2" onClick={recordWebcam.retake}>RETAKE RECORDING</button>
- <button className="bg-green-400 py-2 rounded-[20px] font-bold text-[16px] text-black w-[25%] my-2 mx-2" onClick={recordWebcam.download}>DOWNLOAD RECORDING</button>
- <button className="bg-green-400 py-2 rounded-[20px] font-bold text-[16px] text-black w-[25%] my-2 mx-2" onClick={saveFile}>SAVE FILE TO SERVER</button>
-
-</div>
 </div>
     ) :
-    (<div className="absolute bottom-20">
-      <img src={camera_placeholder} className="w-[40%] ml-[410px] pb-10"/>
-      <button className=" ml-[550px] bg-green-400 py-3 rounded-[20px] font-bold text-[16px] text-black px-20 my-3" onClick={recordWebcam.open}>OPEN CAMERA</button>
+    (<div className="ml-20">
+      <button className="bg-green-400 py-3 rounded-[20px] font-bold text-[16px] text-black px-20 my-3" onClick={recordWebcam.open}>OPEN CAMERA</button>
       </div>) }
     </div>
-   
+
+    { status !== "CLOSED" && (
+    <div id="video_container" className="w-[50%] mt-10">
+    <video ref={recordWebcam.webcamRef} autoPlay muted />
+      <video ref={recordWebcam.previewRef} autoPlay muted loop />
+    </div> )
+}
+
+
     </div>
+    { status === "CLOSED"
+&&
+    (<div className="">
+      <img src={camera_placeholder} className="w-[40%] absolute top-60 right-20 mr-10"/>
+      </div>) }
     </div>
   );
 }
